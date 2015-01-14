b289c4ef131db7bb511a5f7dc6f75275af2bbe94
Roberto Decurnex <decurnex.roberto@gmail.com>
2015-01-14 15:19:56 -0300

    Some samples on how it can be done

---
+++ sample_b/sample.rb
@@ -0,0 +1,31 @@
+class Sample
+
+  include SC
+
+  implements hello: :name
+
+  implements \
+    sum: [:n1, :n2], 
+    rule_of_three: [:a, :b, :c]
+
+  def double n1
+    self.sum(n1, n1)
+  end
+
+end
+
+__END__
+spec_for hello String -> String
+  "Minion"  -> "Say Hello to Minion"
+  "Roberto" -> "Say Hello to Roberto"
+  "Roland"  -> "Say Hello to Roland"
+
+spec_for sum Fixnum, Fixnum -> Fixnum
+  1, 1 -> 2
+  1, 2 -> 3
+  2, 2 -> 4
+
+spec_for rule_of_three Float, Float, Float -> Float
+  1.0, 1.0, 2.0  -> 2.0
+  2.0, 4.0, 3.0  -> 6.0
+  10.0, 2.0, 3.0 -> 0.6
---
+++ sample_c/rsepc/sample_spec.rb
@@ -0,0 +1,21 @@
+describe Sample do
+ 
+  cs_spec_for '#hello String -> String' do 
+    sample.hello('Minion').should == 'Say Hello to Minion'
+    sample.hello('Roberto').should == 'Say Hello to Roberto'
+    sample.hello('Roland').should == 'Say Hello to Roland'
+  end
+
+  cs_spec_for '#sum Fixnum, Fixnum -> Fixnum' do
+    sample.sum(1, 1).should == 2
+    sample.sum(1, 2).should == 3
+    sample.sum(2, 2).should == 4
+  end
+
+  cd_spec_for '#rule_of_three Float, Float, Float -> Float' do
+    sample.rule_of_three(1.0, 1.0, 2.0).should == 2.0
+    sample.rule_of_three(2.0, 4.0, 3.0).should == 6.0
+    sample.rule_of_three(10.0, 2.0, 3.0).should == 0.6
+  end
+
+end
