43219cc938b3c0bc0b90d6be3d9e22cd8e830a13
Roberto Decurnex <decurnex.roberto@gmail.com>
2016-03-24 11:35:30 -0300

    Retab

--- test/minitest/spectro/test_client.rb
+++ test/minitest/spectro/test_client.rb
@@ -7,10 +7,10 @@ class TestSpectro < Minitest::Test
     end
 
     def test_compile
-      Dir.chdir('test/files') do 
+      Dir.chdir('test/files') do
         mock = Minitest::Mock.new
         mock.expect :compile, true
-      
+
         Spectro::Compiler.stub :instance, mock do
           @client.compile
           mock.verify
@@ -18,6 +18,18 @@ class TestSpectro < Minitest::Test
       end
     end
 
-  end
+    def test_compile
+      Dir.mktmpdir do |tmp_dir|
+        Dir.chdir(tmp_dir) do
+          mock = Minitest::Mock.new
+          mock.expect :init, true
 
+          Spectro::Compiler.stub :instance, mock do
+            @client.init
+            mock.verify
+          end
+        end
+      end
+    end
+  end
 end
--- test/minitest/spectro/test_compiler.rb
+++ test/minitest/spectro/test_compiler.rb
@@ -25,7 +25,7 @@ class TestSpectro < Minitest::Test
       Dir.chdir('test/files') do
         @compiler.compile
       end
-      expected_yaml = <<YAML
+      expected_yaml = <<-YAML
 ---
 undefined_sample.rb:
 - !ruby/object:Spectro::Spec
@@ -51,52 +51,52 @@ sample.rb:
     output_type: TrueClass
     params_types:
     - FalseClass
-YAML
+      YAML
       assert File.exists?('test/files/.spectro/undefined.yml'), 'Spectro::Compiler#compile was expected to create an undefined.yml file but it did not.'
       assert_equal YAML.load(expected_yaml), YAML.load_file('test/files/.spectro/undefined.yml')
     end
 
-	def test_compile_without_init
-      Dir.chdir('test/uninitialized_project') do
-	  	assert_raises(SystemExit) do
-        	@compiler.compile
-		end
+    def test_compile_without_init
+        Dir.chdir('test/uninitialized_project') do
+        assert_raises(SystemExit) do
+            @compiler.compile
       end
-	end
+        end
+    end
 
-	def test_init
-		Dir.mktmpdir do |tmp_dir|
-			Dir.chdir(tmp_dir) do
-				@compiler.init
-				assert Dir.exist? '.spectro'
-				assert Dir.exist? '.spectro/cache'
-				assert File.exist? '.spectro/config'
-				assert File.exist? '.spectro/index.yml'
-				assert File.exist? '.spectro/undefined.yml'
-			end
-		end
-	end
+    def test_init
+      Dir.mktmpdir do |tmp_dir|
+        Dir.chdir(tmp_dir) do
+          @compiler.init
+          assert Dir.exist? '.spectro'
+          assert Dir.exist? '.spectro/cache'
+          assert File.exist? '.spectro/config'
+          assert File.exist? '.spectro/index.yml'
+          assert File.exist? '.spectro/undefined.yml'
+        end
+      end
+    end
 
-	def test_init_on_initialized_project
-	  	assert_raises(SystemExit) do
-			Dir.chdir('test/files') do
-				@compiler.init
-			end
-		end
-	end
+    def test_init_on_initialized_project
+        assert_raises(SystemExit) do
+        Dir.chdir('test/files') do
+          @compiler.init
+        end
+      end
+    end
 
-	def test_forced_init_on_project
-		Dir.mktmpdir do |tmp_dir|
-			Dir.chdir(tmp_dir) do
-				@compiler.init
-				File.open('.spectro/index.yml', 'w') do |file|
-					file.write('content')
-				end
-				@compiler.init({f: true})
-				assert(File.read('.spectro/index.yml') === '')
-			end
-		end
-	end
+    def test_forced_init_on_project
+      Dir.mktmpdir do |tmp_dir|
+        Dir.chdir(tmp_dir) do
+          @compiler.init
+          File.open('.spectro/index.yml', 'w') do |file|
+            file.write('content')
+          end
+          @compiler.init({f: true})
+          assert(File.read('.spectro/index.yml') === '')
+        end
+      end
+    end
 
   end
 
